2778cbba1e1dac3a86aeb6959a3e58cbc37bf1d7
David <qiankunli@yahoo.com>
2018-04-30 15:34:40 +0800

    connect to mLab

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var app = express()
 
 var todoRoutes = require('./routes/todo')
 
-mongoose.connect(process.env.databaseURL || 'mongodb://localhost/wepy_todo')
+// mongoose.connect(process.env.databaseURL || 'mongodb://localhost/wepy_todo')
+mongoose.connect('mongodb://david_lee:lleett995162@ds163769.mlab.com:63769/weapptodo')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
